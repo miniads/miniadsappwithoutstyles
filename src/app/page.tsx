8beff2510382344fa1fcac791c7164925de116df
dunnogg <gamemode61@gmail.com>
2024-03-12 17:55:01 +0300

    Update package version

---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React, {useState} from 'react';
+import {TgAdsContainer} from 'miniads';
+
+export default function Home() {
+  const [activeAdType, setActiveAdType] = useState<string | null>(null)
+  const handleBtnClick = (type: string) => {
+    setActiveAdType(type)
+  }
+  return (
+      <div>
+        {activeAdType !== 'full_screen' && (
+            <div className={'buttonwraper'}>
+              <button onClick={() => handleBtnClick('banner')}>Banner</button>
+              <button onClick={() => handleBtnClick('full_screen')}>
+                Full Screen Size
+              </button>
+            </div>
+        )}
+        {activeAdType === 'banner' && (
+            <TgAdsContainer
+                className={'TgAdsContainer'}
+                accessToken={"25f3bf8b-cea4-484f-8701-6f64d162de79"}
+                tgInitData={''}
+                type={activeAdType}
+            />
+        )}
+
+        {activeAdType === 'full_screen' && (
+            <TgAdsContainer
+                className={'TgAdsContainer'}
+                accessToken={"25f3bf8b-cea4-484f-8701-6f64d162de79"}
+                tgInitData={''}
+                type={activeAdType}
+            />
+        )}
+      </div>);
+}
